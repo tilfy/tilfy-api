14665def668b7a9ce9cac3539442efea7321aec9
Marvin Trejo <marvin.trejo@techlatam.la>
2018-11-08 16:39:29 -0600

    add category model and spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_170052) do
+ActiveRecord::Schema.define(version: 2018_11_08_222050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "categories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "username"

848305296ac6f20ff931d918fc1439c85a8b5b39
Marvin Trejo <marvin.trejo@techlatam.la>
2018-11-19 13:21:54 -0600

    Add default url options to env/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # default URL options for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'https://tilfy-api.herokuapp.com' }
 end

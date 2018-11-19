4cd2322fb5ae49d26cda346a7ec99b543522a296
Marvin Trejo <marvin.trejo@techlatam.la>
2018-11-19 13:51:11 -0600

    Disable user confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  include DeviseTokenAuth::Concerns::User
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
 
+  include DeviseTokenAuth::Concerns::User
   # relationships
   has_many :posts, dependent: :destroy
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Devise
-  config.action_mailer.default_url_options = { host: 'https://tilfy-api.herokuapp.com' }
 end

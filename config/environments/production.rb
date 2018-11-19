9ecab210a28b4c2d44d09ebafab56b7b78493883
Marvin Trejo <marvin.trejo@techlatam.la>
2018-11-19 13:31:47 -0600

    Delete default url option from env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # default URL options for the Devise mailer
-  config.action_mailer.default_url_options = { host: 'https://tilfy-api.herokuapp.com' }
 end
